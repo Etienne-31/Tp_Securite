4a44f15d07ecac2de2e8113f565d33066470e5f3
Etienne-31 <90270401+Etienne-31@users.noreply.github.com>
2022-06-02 08:15:20 +0200

    Update slowdown.c

--- TP1/4_LIBRARY_SLOWDOWN/slowdown.c
+++ TP1/4_LIBRARY_SLOWDOWN/slowdown.c
@@ -1,24 +1,53 @@
 #include <stdio.h>
 #include "cache.h"
 
-int main(int argc, char **argv) {
+int main(int ac, char **av) {
+	
+	char * name = av[1];
+	uint32_t offset = 0;
 	
 	// Add arguments of map_offset function //
-	void* ptr = map_offset( /** arg 1 **/ , /* arg 2 */ );
-	if (ptr == NULL) {
-		// Write your code below //
-		
-		
+	if(ac != 0 && ac > 2) offset = strtol(av[2],NULL,16);
+	
+	// Add arguments of map_offset function //
+	void* ptr = map_offset( name, offset );
+	void * handle = dlopen (name, RTLD_LAZY);
+	
+	if (!handle) {
+		fprintf (stderr, "%s\n", dlerror());
+		exit(1);
 	}
 	
+	dlerror();    // Clear any existing error //
+	
+	void * modmult = dlsym(handle, "modmult");
+	char * error;
+	
+	if ((error = dlerror()) != NULL) {
+		fprintf (stderr, "%s\n", error);
+		return 1;
+	}
+	
+	if (modmult == NULL) {
+		// Write your code below //
+		exit(1);
+	}
+	
+	//int offs=ptr-modmult;
+	printf("ptr lib : %p\n",ptr);
+	printf("ptr modmul : %p\n",modmult);
+	printf("ptr offset : %x\n",offset);
+	printf("ptr handle : %p\n",handle);
 	
 	while(1) {
 		// Write your code below //
-		
-		
-		
+		clflush(ptr);
 	}
+	/*resultat : 
+	sans slowdown : 15944/30638/148
+	slowdown : 288958/480532/1726s
 	
+	*/
 	return 0;
 }
 
